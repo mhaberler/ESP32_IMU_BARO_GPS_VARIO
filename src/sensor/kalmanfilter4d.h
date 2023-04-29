3c6fe123f0e84cc3e3d781aa0569df01a8c68548
Michael Haberler <git@mah.priv.at>
2023-04-29 18:21:22 +0200

    more than one include file...

--- src/sensor/kalmanfilter4d.h
+++ src/sensor/kalmanfilter4d.h
@@ -3,7 +3,7 @@
 
 void kalmanFilter4d_configure(float aVariance, float kAdapt, float zInitial, float vInitial, float aInitial);
 void kalmanFilter4d_predict(float dt);
-void kalmanFilter4d_update(float zm, float am, float* pz, float* pv);
+void kalmanFilter4d_update(float zm, float am, float *pz, float *pv, float *pa);
 
 #endif
 
